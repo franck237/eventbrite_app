0998da621764cba08b20270e1ae86eb003ffdffe
franck237 <franck.kamanou@gmail.com>
2019-08-13 20:05:41 +0200

    ca Soule

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@
 	random_user_list = []
 10.times do
 	a = Faker::Alphanumeric.alpha(number: 10)
-	random_user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 9, supplemental: false, random_words_to_add: 10), email: "#{a}@yopmail.com", password: Faker::Internet.password(min_length: 10, max_length: 20))
+	random_user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 9, supplemental: false, random_words_to_add: 10), email: "#{a}@yopmail.com", encrypted_password: Faker::Internet.password(min_length: 10, max_length: 20))
 	random_user_list << random_user
 end
