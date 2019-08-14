3ae27949056da5f0785607137f032ef87ffa7516
franck237 <franck.kamanou@gmail.com>
2019-08-14 06:36:23 +0200

    Ajout de la Page d'accueil

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,18 @@
 class EventsController < ApplicationController
   def index
+  	#permet d'afficher une alerte à l'utilisateur
+    puts flash[:alert]
+
+    puts "-" * 60
+    puts "Voici le nom du dernier auteur :"
+    @events_list = Event.all
+    puts @events_list[-1].user.last_name
+    puts "-" * 60
+  end
+
+  def new
+  end
+
+  def show  	
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+User.destroy_all
 
-#User.destroy_all
 
 #Création de 10 utilisateurs aléatoires portant chacun une des villes précedemment crées en argument foreign key
 	random_user_list = []
 10.times do
 	a = Faker::Alphanumeric.alpha(number: 10)
-	random_user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 9, supplemental: false, random_words_to_add: 10), email: "#{a}@yopmail.com", encrypted_password: Faker::Internet.password(min_length: 10, max_length: 20))
+	random_user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 9, supplemental: false, random_words_to_add: 10), email: "#{a}@yopmail.com", password: Faker::Internet.password(min_length: 10, max_length: 20))
 	random_user_list << random_user
+end
+
+#Création de 20 event aléatoires portant chacun un user aléatoire précedemment crée en argument foreign key
+	random_events_list = []
+20.times do
+	random_event = Event.create(start_date: Faker::Time.forward(days: 365), duration: rand(4..50)*5, title: Faker::Lorem.sentence(word_count: 5, supplemental: false, random_words_to_add: 3), description: Faker::Lorem.sentence(word_count: 40, supplemental: false, random_words_to_add: 10), price: rand(100), location: Faker::Nation.capital_city, user_id: random_user_list.sample.id)
+	random_events_list << random_event
 end
